12cd7536b0106478a941f88fad9913e2e7383682
zmercedes <zmercedes89@gmail.com>
2018-01-08 11:36:10 -0500

    added html response

--- src/sitelist/views.py
+++ src/sitelist/views.py
@@ -3,5 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-	return HttpResponse("hello")
+	html = """<!doctype html>
+	<html lang="en">
+	<head>
+	</head>
+	<body>
+	<h1> smdftb </h1>
+	</body>
+	</html>
+	"""
+	return HttpResponse(html)
 	#return render(request,"home.html",{}) # response
