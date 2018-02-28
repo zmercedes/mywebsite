d82173877044324ac63647f391bfd1e4ff337878
zmercedes <zmercedes89@gmail.com>
2018-02-28 15:37:49 -0500

    adding project models

--- src/sitelist/models.py
+++ src/sitelist/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+'''
+
+'''
+class Project(models.Model):
+	name           = models.CharField(max_length = 50)
+	description    = models.CharField(max_length = 500)
--- src/sitelist/views.py
+++ src/sitelist/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
 
+from .models import Project
+
 class BaseView(TemplateView):
 	template_name = "base.html"
